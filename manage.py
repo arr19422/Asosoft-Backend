8ce23ce2e3f40574819455f03ef8040be43d03bd
Oscar Saravia <46576030+oscarsaravia@users.noreply.github.com>
2021-04-25 14:55:33 -0600

    fix: problem with manage.py solved

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asosoftApp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AsosoftApp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
