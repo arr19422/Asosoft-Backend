1f0e939b62d15c1824fa17df782410cec8f3d8f4
arr19422 <arr19422@uvg.edu.gt>
2021-05-03 17:04:43 -0600

    UserModel changes, gitignore corrections

--- api/Models/matchModel.py
+++ api/Models/matchModel.py
@@ -5,6 +5,6 @@ from .tournamentModel import Tournament
 
 class Match(models.Model):
     match_date = models.DateField(blank=False)
-    local_team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False)
-    visiting_team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False)
+    local_team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False, related_name='local_teams')
+    visiting_team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False, related_name='visiting_teams')
     tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, blank=False)
--- api/Models/userModel.py
+++ api/Models/userModel.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 from .teamModel import Team
 
-
-class Users(AbstractUser):
+class Users(models.Model):
     name = models.CharField(max_length=32, blank=False)
     last_name = models.CharField(max_length=32, blank=False)
     email = models.CharField(max_length=32, blank=False)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, default=None)
