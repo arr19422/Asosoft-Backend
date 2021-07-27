bd934a0526969e99a4d9a30093818a5d71d9d059
arr19422 <arr19422@uvg.edu.gt>
2021-07-26 19:55:34 -0600

    settings for heroku

--- AsosoftApp/settings.py
+++ AsosoftApp/settings.py
@@ -12,13 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
 
+# Configure Django App for Heroku.
 
 load_dotenv()
-env_path = Path('.')/'.env'
+env_path = Path('') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,7 +89,6 @@ REST_FRAMEWORK = {
     ]
 }
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -148,3 +145,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+
+import django_heroku
+
+django_heroku.settings(locals())
